0995b20f33c21dd87e977ab2e8e0a63a01777f52
Jon-Magne Aa. Jønholt <jon-magneaa.jonholt@Jon-Magnes-MacBook-Pro.local>
2019-11-29 09:45:20 +0100

    Updates

---
+++ precache-manifest.47ff227e84ade24c3b680cdcb5df2148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782c189185b14788a10580735c1a509e",
+    "url": "/temaoppgave/index.html"
+  },
+  {
+    "revision": "5fd1a2312d8e8011ff0a",
+    "url": "/temaoppgave/static/css/main.f42e1c2d.chunk.css"
+  },
+  {
+    "revision": "6f3621f6d99ad8b2bd0f",
+    "url": "/temaoppgave/static/js/2.e833feb4.chunk.js"
+  },
+  {
+    "revision": "5fd1a2312d8e8011ff0a",
+    "url": "/temaoppgave/static/js/main.b8db64df.chunk.js"
+  },
+  {
+    "revision": "86bbbb1e52103a3739c8",
+    "url": "/temaoppgave/static/js/runtime-main.b74b5b47.js"
+  }
+]);
